4949ad1765048ff64275470cfd42b036ebd1a210
Beccapwd <rjstilley@gmail.com>
2021-05-17 18:08:47 -0500

    added code

--- static/js/logic.js
+++ static/js/logic.js
@@ -9,7 +9,7 @@ function size(Strength) {
 }
 
 function color(how_deep) {
-    if (Depth > 90) return "red"
+    if (how_deep > 90) return "red"
     else if (how_deep > 70) return "dark orange"
     else if (how_deep > 50) return "orange"
     else if (how_deep > 30) return "yellow"
@@ -40,7 +40,7 @@ function createFeatures(earthquakeData) {
     createMap(EQ);
 }
 function createMap(EQ) {
-    var viewStreet = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
         tileSize: 512,
         maxZoom: 18,
@@ -48,6 +48,13 @@ function createMap(EQ) {
         id: "mapbox/streets-v11",
         accessToken: API_KEY
     });
+    var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "dark-v10",
+        accessToken: API_KEY
+    });
+
     var baseMaps = {
         "Street Map": streetmap,
         "Dark Map": darkmap
