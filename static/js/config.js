7d45b7cdc7266851ec6152653bc36075f6604be3
Beccapwd <rjstilley@gmail.com>
2021-05-19 18:22:14 -0500

    api key config

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,3 @@
 // API key
-const API_KEY = "Ypk.eyJ1IjoiYmVuamFtZW5hbGZvcmQiLCJhIjoiY2p2NWg5enJkMHVyMzQ0bWtmY2FtNmM5bCJ9.mFsGlikCvtB_-6xIFCVPiQ";
+// const API_KEY = "Ypk.eyJ1IjoiYmVuamFtZW5hbGZvcmQiLCJhIjoiY2p2NWg5enJkMHVyMzQ0bWtmY2FtNmM5bCJ9.mFsGlikCvtB_-6xIFCVPiQ";
+const API_KEY = "pk.eyJ1IjoiYnJvb2thbm4iLCJhIjoiY2tvcDI5ZzQ4MDF6eTJxbGM0YzJ6d3N3YiJ9.UPSAHkGUAQB4nJo0NnXv9g"
